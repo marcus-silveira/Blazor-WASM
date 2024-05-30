b41635ad4ecd7c88bb724dd1c2fcbb0b2bf32fca
Marcus Silveira <marcusvinicius.767.mv@gmail.com>
2024-05-30 13:55:38 -0300

    Criado serviço para conectar a API

---
+++ ScreenSound.WASM/Services/ArtistClient.cs
@@ -0,0 +1,41 @@
+﻿using ScreenSound.WASM.Requests;
+using ScreenSound.WASM.Response;
+using System.Net.Http.Json;
+
+namespace ScreenSound.WASM.Services;
+
+public class ArtistClient
+{
+	private readonly HttpClient _httpClient;
+
+	public ArtistClient(IHttpClientFactory factory)
+	{
+		_httpClient = factory.CreateClient("ScreenSound.ServerAPI");
+	}
+
+	public async Task<ICollection<ArtistResponse>?> GetArtistsAsync()
+	{
+		var result = await _httpClient.GetFromJsonAsync<ICollection<ArtistResponse>>("artistas");
+		return result;
+	}
+
+	public async Task AddArtistAsync(ArtistRequest artist)
+	{
+		await _httpClient.PostAsJsonAsync("artistas", artist);
+	}
+
+	public async Task<ArtistResponse?> GetArtistByName(string name)
+	{
+		return await _httpClient.GetFromJsonAsync<ArtistResponse>($"artistas/{name}");
+	}
+	public async Task DeleteArtistAsync(int id)
+	{
+		await _httpClient.DeleteAsync($"artistas/{id}");
+	}
+
+	public async Task UpdateArtistAsync(ArtistRequestEdit artist)
+	{
+		await _httpClient.PutAsJsonAsync($"artistas", artist);
+	}
+}
+
---
+++ ScreenSound.WASM/Services/MusicClient.cs
@@ -0,0 +1,18 @@
+﻿using ScreenSound.WASM.Response;
+using System.Net.Http.Json;
+
+namespace ScreenSound.WASM.Services;
+
+public class MusicClient
+{
+	private readonly HttpClient _httpClient;
+	public MusicClient(IHttpClientFactory factory)
+	{
+		_httpClient = factory.CreateClient("ScreenSound.ServerAPI");
+	}
+
+	public async Task<ICollection<MusicResponse>?> GetMusicsAsync()
+	{
+		return await _httpClient.GetFromJsonAsync<ICollection<MusicResponse>>("musicas");
+	}
+}
